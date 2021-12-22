dbdc04ea3b2107235c52f54745c0c2f5d7f5550a
leocrossman <lkcrossman@gmail.com>
2021-12-22 13:15:59 -0500

    merge sort done/all tests passing

--- util/mergeSort.js
+++ util/mergeSort.js
@@ -1,12 +1,32 @@
 /**
+ * O(nlogn) time
+ * O(logn) space
  * @param {Array} arr
  * @returns {Array}
  */
 const mergeSort = (arr) => {
-  return arr;
+  if (arr.length < 2) return arr;
+  const mid = Math.floor(arr.length / 2);
+  const left = mergeSort(arr.slice(0, mid));
+  const right = mergeSort(arr.slice(mid));
+  return merge(left, right);
 };
 
-let arr = [5, 2, 3, 1, 4];
-console.log(mergeSort(arr)); // [1,2,3,4,5]
+const merge = (left, right) => {
+  let L = 0,
+    R = 0;
+  const merged = [];
+  while (L < left.length && R < right.length) {
+    if (left[L] < right[R]) merged.push(left[L++]);
+    else merged.push(right[R++]);
+  }
+  while (L < left.length) {
+    merged.push(left[L++]);
+  }
+  while (R < right.length) {
+    merged.push(right[R++]);
+  }
+  return merged;
+};
 
 module.exports = mergeSort;
