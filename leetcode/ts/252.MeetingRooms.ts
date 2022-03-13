28314cf6fb086846c77f10297a2591231efa0c99
leocrossman <38471886+leocrossman@users.noreply.github.com>
2022-03-13 15:01:05 -0400

    meeting rooms one done

--- leetcode/ts/252.MeetingRooms.ts
+++ leetcode/ts/252.MeetingRooms.ts
@@ -1,4 +1,12 @@
-function canAttendMeetings(intervals: number[][]): boolean {}
+function canAttendMeetings(intervals: number[][]): boolean {
+  intervals.sort((a, b) => a[0] - b[0]);
+  for (let i = 1; i < intervals.length; i++) {
+    const prev: number[] = intervals[i - 1];
+    const curr: number[] = intervals[i];
+    if (prev[1] > curr[0]) return false;
+  }
+  return true;
+}
 
 console.log(
   canAttendMeetings([
