c279442833e39ae2de8edf5139f642c79971889a
leocrossman <38471886+leocrossman@users.noreply.github.com>
2022-06-20 09:23:56 -0400

    added 279.PerfectSquares.ts

---
+++ leetcodeProblems/ts/28.ImplementstrStr.ts
@@ -0,0 +1,8 @@
+/*
+Implement strStr().
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+*/
+
+function strStr(haystack: string, needle: string, fromIdx: number = 0):number {
+  return 0;
+}
