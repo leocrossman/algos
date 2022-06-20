67c8add2711a7d1a42d3a25dd173477a2b35980c
leocrossman <38471886+leocrossman@users.noreply.github.com>
2022-06-20 11:43:04 -0400

    added dfs on n-ary tree solution

--- leetcodeProblems/ts/279.PerfectSquares.ts
+++ leetcodeProblems/ts/279.PerfectSquares.ts
@@ -8,7 +8,7 @@ A perfect square is an integer that is the square of an integer; in other words,
  *Time: O(n * sqrt(1/2))
  *Space: O(n)
 */
-const numSquares = (n: number): number => {
+const numSquaresDP = (n: number): number => {
   const dp = Array(n + 1).fill(n);
   dp[0] = 0;
   for (let target = 1; target < n + 1; target++) {
@@ -22,3 +22,31 @@ const numSquares = (n: number): number => {
 }
 
 
+/**
+ * iteratively deepening DFS on an n-ary tree
+ * Time: O(n^h/2) where h is the number of recursive calls made
+ * Space: O(n^1/2)
+ */
+const numSquares = (n: number): number => {
+  const squares = getSquares(n);
+  for (let i = 1; i < n + 1; i++) {
+    if (isDividedBy(squares, n, i)) return i;
+  }
+  return -1;
+}
+
+const isDividedBy = (squares: Set<number>, n: number, count: number): boolean => {
+  if (count === 1) return squares.has(n);
+  for (const k of squares) {
+    if (isDividedBy(squares, n - k, count - 1)) return true;
+  }
+  return false;
+}
+
+const getSquares = (n): Set<number> => {
+  const squares = new Set<number>();
+  for (let i = 0; i < Math.sqrt(n) + 1; i++) {
+    squares.add(i * i);
+  }
+  return squares;
+}
